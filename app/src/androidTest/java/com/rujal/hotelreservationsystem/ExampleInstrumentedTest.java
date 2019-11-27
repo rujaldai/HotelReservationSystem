9c17ed83ec6d42b8d8bcc49f89a535c4d3202adc
rujaldai@gmail.com <rujaldai@gmail.com>
2019-11-27 23:07:24 +0545

    rename app title

--- app/src/androidTest/java/com/rujal/hotelreservationsystem/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/rujal/hotelreservationsystem/ExampleInstrumentedTest.java
@@ -22,6 +22,6 @@ public class ExampleInstrumentedTest {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
 
-        assertEquals("com.rujal.hotelreservationsyste", appContext.getPackageName());
+        assertEquals("com.rujal.hotelreservationsystem", appContext.getPackageName());
     }
 }
