ac20766a02b354761a0d25cb99b5854a6c59b606
rujaldai@gmail.com <rujaldai@gmail.com>
2019-11-27 22:44:14 +0545

    use lamdas because it's cooler, change room costs

--- app/src/main/java/com/rujal/hotelreservationsystem/RoomType.java
+++ app/src/main/java/com/rujal/hotelreservationsystem/RoomType.java
@@ -1,10 +1,12 @@
 package com.rujal.hotelreservationsystem;
 
+import java.util.Arrays;
+
 enum RoomType {
     NONE(" -- Select Room Type -- ", 0.0F),
-    DELUXE("Deluxe", 1000.0F),
-    PREMIUM("Premium", 1500.0F),
-    PRESIDENTIAL("Presidential", 2000.0F);
+    DELUXE("Deluxe", 2000.0F),
+    PREMIUM("Premium", 4000.0F),
+    PRESIDENTIAL("Presidential", 5000.0F);
 
     private String displayText;
     private  Float cost;
@@ -15,12 +17,10 @@ enum RoomType {
     }
 
     public static RoomType valueOfLabel(String label) {
-        for (RoomType roomType : values()) {
-            if (roomType.getDisplayText().equals(label)) {
-                return roomType;
-            }
-        }
-        return null;
+        return Arrays.stream(values())
+                .filter(roomType -> roomType.getDisplayText().equals(label))
+                .findFirst()
+                .orElse(null);
     }
 
     public String getDisplayText() {
